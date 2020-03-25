9167074a65539a7c10cb7830762ad84fe5125520
jdinhify <jdinhify@gmail.com>
2020-03-25 22:01:09 +1100

    ✨  add surfingkeys config

---
+++ configs/surfingkeys.js
@@ -0,0 +1,3 @@
+settings.blacklistPattern = /.*mail.google.com.*|.*drive.google.com.*|.*docs.google.com.*|.*outlook.office.com.*|.*typeform.com.*|.*app.slack.com.*|.*github.com.*|.*localhost.*/i;
+
+unmapAllExcept(['f', 'gg', 'G', 'd', 'u', 'h', 'j', 'k', 'l']);

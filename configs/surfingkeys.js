990cc424f0abfccafd4c376ff826622b3aa945ae
JD <jdinhify@gmail.com>
2021-10-07 09:17:47 +1100

    remove github from surfingkeys ignore

--- configs/surfingkeys.js
+++ configs/surfingkeys.js
@@ -1,3 +1,4 @@
-settings.blacklistPattern = /.*mail.google.com.*|.*drive.google.com.*|.*docs.google.com.*|.*outlook.office.com.*|.*typeform.com.*|.*app.slack.com.*|.*github.com.*|.*localhost.*/i;
+settings.blacklistPattern =
+  /.*mail.google.com.*|.*drive.google.com.*|.*docs.google.com.*|.*outlook.office.com.*|.*typeform.com.*|.*app.slack.com.*|.*localhost.*/i
 
-unmapAllExcept(['f', 'gg', 'G', 'd', 'u', 'h', 'j', 'k', 'l']);
+unmapAllExcept(['f', 'gg', 'G', 'd', 'u', 'h', 'j', 'k', 'l'])
